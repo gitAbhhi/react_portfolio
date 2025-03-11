0e21dae75b347375eb85c976680d69f2b2096179
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2025-03-11 11:11:39 +0530

    add skills

--- src/assets/Working_data.js
+++ src/assets/Working_data.js
@@ -2,58 +2,58 @@ import amazom from '../assets/amazom.png'
 import tictactoe from '../assets/tictactoe.png'
 import stonepaper from '../assets/stonepaper.png'
 import netflix from '../assets/Netflix.png'
-import cconverter from '../assets/cc.webp'
+import cconverter from '../assets/cc.png'
 import weather from '../assets/weather.png'
 import twitter from '../assets/Twitter.png'
 import todo from '../assets/Todo.png'
 const mywork_data = [
-    {
-      w_no: 1,
+  {
+    w_no: 5,
+    w_name: "X Clone",
+    w_img: twitter,
+     w_link: "https://twitter-clone-drab-five.vercel.app/",
+  },
+  {
+    w_no: 7,
+    w_name: "Todo App",
+    w_img: todo,
+    w_link: "https://todo-app-omega-seven-58.vercel.app/",
+  },
+  {
+    w_no: 6,
+    w_name: "Weather Web",
+    w_img: weather,
+    w_link: "https://weather-app-five-coral-79.vercel.app/",
+  },
+  {
+    w_no: 8,
+    w_name: "Curency Converter",
+    w_img: cconverter,
+     w_link: "https://currency-converter-black-nine-42.vercel.app/",
+  },
+  {
+    w_no: 2,
+    w_name: "Tic Tac Toe",
+    w_img: tictactoe,
+    w_link: "https://gitabhhi.github.io/Tic-Tac-Toe/",
+  },
+  {
+    w_no: 3,
+    w_name: "Stone Paper",
+    w_img: stonepaper,
+    w_link: "https://gitabhhi.github.io/Rock-Paper-Scissors/",
+  },
+  {
+    w_no: 1,
       w_name: "Amazon Clone",
       w_img: amazom,
       w_link: "https://amazon.com",
-    },
-    {
-      w_no: 2,
-      w_name: "Tic Tac Toe",
-      w_img: tictactoe,
-      w_link: "https://gitabhhi.github.io/Tic-Tac-Toe/",
-    },
-    {
-      w_no: 3,
-      w_name: "Stone Paper",
-      w_img: stonepaper,
-      w_link: "https://gitabhhi.github.io/Rock-Paper-Scissors/",
     },
     {
       w_no: 4,
       w_name: "Netflix Clone",
       w_img: netflix,
        w_link: "https://amazon.com",
-    },
-    {
-      w_no: 5,
-      w_name: "X Clone",
-      w_img: twitter,
-       w_link: "https://gitabhhi.github.io/twitter_clone/",
-    },
-    {
-      w_no: 6,
-      w_name: "Weather Web",
-      w_img: weather,
-       w_link: "https://gitabhhi.github.io/Weather_app/",
-    },
-    {
-      w_no: 7,
-      w_name: "Todo App",
-      w_img: todo,
-       w_link: "https://gitabhhi.github.io/todo_app/",
-    },
-    {
-      w_no: 8,
-      w_name: "Curency Converter",
-      w_img: cconverter,
-       w_link: "https://gitabhhi.github.io/Currency_Converter/",
     },
   ];
   export default mywork_data;
--- src/assets/skill.js
+++ src/assets/skill.js
@@ -5,29 +5,41 @@ import node from "../assets/nodejs.png"
 import java from "../assets/java.png"
 import python from "../assets/python.png"
 import postman from "../assets/postman.png"
+import html from "../assets/html.png"
+import css from "../assets/css.png"
+import tailwind from "../assets/tailwind.png"
+import bootstrap from "../assets/bootstrap.png"
+import mysql from "../assets/mysql.png"
+import Jupyter from "../assets/jupyter.png"
+import linux from "../assets/linux.png"
+import colab from "../assets/colab.png"
+import figma from "../assets/figma.png"
+import git from "../assets/git.png"
+import github from "../assets/github.png"
+import vercel from "../assets/vercel.png"
 
 
 const skill_data=[
     {
-        category:"Mern Stack",
-        technologies:["Mongodb","Reactjs","Expressjs","Nodejs"],
-        image:[mdb,react,express,node]
+        category:"Frontend",
+        technologies:["HTML","CSS","Tailwind CSS","Bootstrap","Reactjs"],
+        image:[html,css,tailwind,bootstrap,react]
     },
    
       {
-        category: "Java Full stack",
-        technologies: ["Java"],
-        image:[java]
+        category: "Backend",
+        technologies: ["MongoDB","MySQL","Expressjs","Nodejs","Java"],
+        image:[mdb,mysql,express,node,java]
       },
       {
         category: "Machine Learning",
-        technologies: ["Python"],
-        image:[python]
+        technologies: ["Python","Jupyter","Google Colab"],
+        image:[python,Jupyter,colab]
       },
       {
         category: "Others",
-        technologies: ["postman"],
-        image:[postman]
+        technologies: ["postman","Linux","Figma","Git","GitHub","Vercel"],
+        image:[postman,linux,figma,git,github,vercel]
       },
 
 ]
