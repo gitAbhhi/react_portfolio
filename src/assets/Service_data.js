01f3254f9895bb95b39c35512f075a24a3886a5d
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2025-03-24 20:38:34 +0530

    add services

--- src/assets/Service_data.js
+++ src/assets/Service_data.js
@@ -1,23 +1,28 @@
 const Services_Data = [
     {
       s_no: "01",
-      s_name: "Web design",
-      s_desc: "Web development is the process of building, programming..."
+      s_name: "Web Development",
+      s_desc: " Building responsive and dynamic websites using HTML, CSS, JavaScript, React, and backend technologies like Node.js."
     },
     {
       s_no: "02",
-      s_name: "Graphics design",
-      s_desc: "Web development is the process of building, programming..."
+      s_name: "Software Development",
+      s_desc: "Building scalable software applications using Java"
     },
     {
       s_no: "03",
       s_name: "Social media",
-      s_desc: "Web development is the process of building, programming..."
+      s_desc: "Creating engaging videos, memes, and aesthetic nature reels."
     },
     {
-      s_no: "04",
-      s_name: "App design",
-      s_desc: "Web development is the process of building, programming..."
+      s_no: "04",  
+      s_name: "UI/UX Design ",
+      s_desc: "Designing user-friendly and visually appealing interfaces using Figma"
+    },
+    {
+      s_no: "05",
+      s_name: "IoT Development",
+      s_desc: "Building smart systems using Raspberry Pi, Arduino, and IoT protocols."
     }
   ];
   export default Services_Data;
